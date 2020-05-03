fd40e02fa495e4587c0470f3760420b9b40f5895
bugbug-lou <58448037+bugbug-lou@users.noreply.github.com>
2020-05-03 13:50:12 +0800

    Update dotplot.py

--- dotplot.py
+++ dotplot.py
@@ -1,4 +1,3 @@
-# import
 import numpy as np
 import datetime
 import random
@@ -11,12 +10,14 @@ from lempel_ziv_complexity import lempel_ziv_complexity
 import collections
 import argparse
 
+
 # functions
 def array_to_string(x):
     y = ''
     for l in x:
         y += str(int(l))
     return y
+
 
 def analyze(x):
     a = x.size()[0]
@@ -36,6 +37,47 @@ def get_max_freq(x):
     for f in list(T.keys()):
         if T[f] == a:
             return f
+
+
+def get_LVComplexity(x):
+    ones = torch.ones(len(x))
+    zeros = torch.zeros(len(x))
+    if torch.all(torch.eq(x, ones)) or torch.all(torch.eq(x, zeros)):
+        return len(x)
+    else:
+        with torch.no_grad():
+            a = N_w(x)
+            y = np.asarray(x)
+            y = y[::-1]
+            b = N_w(y)
+        return np.log2(len(x)) * (a + b) / 2
+
+
+def N_w(S):
+    # get number of words in dictionary
+    i = 0
+    C = 1
+    u = 1
+    v = 1
+    vmax = v
+    while u + v < len(S):
+        if S[i + v] == S[u + v]:
+            v = v + 1
+        else:
+            vmax = max(v, vmax)
+            i = i + 1
+            if i == u:
+                C = C + 1
+                u = u + vmax
+                v = 1
+                i = 0
+                vamx = v
+            else:
+                v = 1
+    if v != 1:
+        C = C + 1
+    return C
+
 
 def train(model, loss, optimizer, inputs, labels):
     model.train()
@@ -69,13 +111,14 @@ def predict(model, inputs):
     logits = model.forward(inputs)
     return logits
 
+
 # some parameters
-n = 7 # dimension of input data, user-defined
+n = 7  # dimension of input data, user-defined
 m = 2 ** n  # number of data points
 m_2 = 2 ** (n - 1)
 m_3 = 2 ** (n - 2)
-predict_threshold = 0.001 # training accuracy threshold
-MC_num = 10 ** 3 # number of random initialization of models
+predict_threshold = 0.001  # training accuracy threshold
+MC_num = 10 ** 4  # number of random initialization of models
 neu = 40  # neurons per layer
 mean = 0.0  # mean of initialization
 scale = 10  # STD of initialization
@@ -95,8 +138,8 @@ XTest = torch.zeros([m_2, n])
 YTrain = torch.zeros(m_2)
 YTest = torch.zeros(m_2)
 for i in range(m_2):
-    XTrain[i,:] = data[2*i,:]
-    XTest[i, :] = data[2 * i + 1, :]
+    XTrain[i, :] = data[i, :]
+    XTest[i, :] = data[i + m_2, :]
 
 target = torch.zeros(m)
 for i in range(m_3):
@@ -105,8 +148,8 @@ for i in range(m_3):
     target[i + m_3] = 0
     target[i + m_3 + m_2] = 0
 for i in range(m_2):
-    YTrain[i] = target[2 * i]
-    YTest[i] = target[2 * i + 1]
+    YTrain[i] = target[i]
+    YTest[i] = target[i + m_2]
 YTrain = YTrain.long()
 YTest = YTest.long()
 
@@ -118,7 +161,7 @@ outputs = {}
 loss = torch.nn.CrossEntropyLoss(size_average=True)
 
 for i in range(MC_num):
-    if i%100 == 0:
+    if i % 1000 == 0:
         print(str(i) + 'aha!')
     model1 = torch.nn.Sequential()
     model1.add_module('FC1', torch.nn.Linear(n, neu))
@@ -149,48 +192,41 @@ for i in range(MC_num):
         pr2 = get_error(model2, XTrain, YTrain, m_2)
 
     # record test set:
-    a = array_to_string(analyze(predict(model1, XTest)))
-    b = array_to_string(analyze(predict(model2, XTest)))
-    c = get_error(model1, XTest, YTest, 1)
-    d = get_error(model2, XTest, YTest, 1)
+    k1 = analyze(predict(model1, data))
+    k2 = analyze(predict(model2, data))
+    a = array_to_string(k1)
+    b = array_to_string(k2)
+    c = get_LVComplexity(k1)
+    d = get_LVComplexity(k2)
     if a in outputs.keys():
-        outputs[a] = (outputs[a][0] + float(1/MC_num), outputs[a][1], c)
+        outputs[a] = (outputs[a][0] + float(1 / MC_num), outputs[a][1], c)
     else:
-        outputs[a] = (1/MC_num, 0, c)
+        outputs[a] = (1 / MC_num, 0, c)
     if b in outputs.keys():
-        outputs[b] = (outputs[b][0], outputs[b][1] + float(1/MC_num), d)
+        outputs[b] = (outputs[b][0], outputs[b][1] + float(1 / MC_num), d)
     else:
-        outputs[b] = (0, float(1/MC_num), d)
+        outputs[b] = (0, float(1 / MC_num), d)
 
     del model1
     del model2
 
 # plot
-color = ['darkgreen', 'yellowgreen', 'khaki', 'yellow', 'gold', 'orange', 'orangered', 'red', 'red', 'red', 'red']
-Z = torch.arange(0,1,0.001)
+color = ['black', 'purple', 'darkblue', 'darkgreen', 'yellow', 'orange', 'orangered', 'red', 'red', 'red', 'red', 'red', 'red']
+Z = torch.arange(0, 1, 0.001)
 
-for i in range(11):
-    if i < 10:
-        l = [x for (x, y, z) in list(outputs.values()) if z==i]
-        p = [y for (x, y, z) in list(outputs.values()) if z==i]
-        plt.scatter(p, l, color=color[i], alpha=1.0, label=str(i))
-    else:
-        l = [x for (x, y, z) in list(outputs.values()) if z >= 10]
-        p = [y for (x, y, z) in list(outputs.values()) if z >= 10]
-        plt.scatter(p, l, color=color[i], alpha=1.0, label='>= 10')
+for i in range(13):
+    l = [x for (x, y, z) in list(outputs.values()) if i * 10 <= z < (i + 1) * 10]
+    p = [y for (x, y, z) in list(outputs.values()) if i * 10 <= z < (i + 1) * 10]
+    plt.scatter(p, l, color=color[i], alpha=1.0, label='complexity range:' + str(10 * i) + '-' + str(10 * (i + 1)))
 
-plt.plot(Z, Z, color = 'blue')
-plt.xlim(1/MC_num, 1)
-plt.ylim(1/MC_num, 1)
+
+plt.plot(Z, Z, color='blue')
+plt.xlim(1 / MC_num, 1)
+plt.ylim(1 / MC_num, 1)
 plt.xlabel('P(f) SGD C_E BN')
 plt.ylabel('P(f) SGD C_E NN')
 plt.legend(loc="upper right")
 plt.xscale('log')
 plt.yscale('log')
 plt.show()
-
-
-
-
-
 
